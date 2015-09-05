584690379247fadea0e33821f45a05e439ec1de4
countdracular <lizikuan96@163.com>
2015-09-05 20:42:34 +0800

    Put map page to new route '/map'

--- routes/map.js
+++ routes/map.js
@@ -1,9 +1,12 @@
+/**
+ * Created by Administrator on 2015/9/3.
+ */
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET map page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('map');
 });
 
 module.exports = router;
